0355a73a98c7fb59f3aa0f134fc3c38828b6f80d
Volcomix <Volcomix@users.noreply.github.com>
2025-08-02 16:11:09 +0200

    Implement download

--- download-dawn.ts
+++ download-dawn.ts
@@ -1,4 +1,14 @@
-process.loadEnvFile("./.env.local");
+import { createWriteStream } from "node:fs";
+import { mkdir } from "node:fs/promises";
+import { dirname } from "node:path";
+import { Readable } from "node:stream";
+import { pipeline } from "node:stream/promises";
+import { ReadableStream } from "node:stream/web";
+import { fileURLToPath } from "node:url";
+
+const downloadDir = "downloads";
+
+process.loadEnvFile(".env.local");
 
 const githubToken = process.env.GITHUB_TOKEN;
 if (!githubToken) {
@@ -32,7 +42,23 @@ for (const tag of tags) {
     `Found artifact: ${artifact.name} (${artifact.size_in_bytes} bytes)`
   );
 
+  const archiveResponse = await fetchDawnGitHub(
+    `actions/artifacts/${artifact.id}/zip`
+  );
+
+  if (!archiveResponse.body) {
+    throw new Error(
+      `Failed to download artifact ${artifact.name}: ${archiveResponse.statusText}`
+    );
+  }
+
+  console.log(`Downloading artifact ${artifact.name}...`);
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  await mkdir(`${__dirname}/${downloadDir}`, { recursive: true });
+  await pipeline(
+    Readable.fromWeb(archiveResponse.body as ReadableStream),
+    createWriteStream(`${__dirname}/${downloadDir}/${artifact.name}.zip`)
+  );
+
   break;
 }
-
-export {};
