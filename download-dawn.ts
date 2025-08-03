278ca958e412b5d3bba4950452e175c322d38475
Volcomix <Volcomix@users.noreply.github.com>
2025-08-03 20:18:47 +0200

    Download dawn.json

--- download-dawn.ts
+++ download-dawn.ts
@@ -1,5 +1,5 @@
 import { createWriteStream } from "node:fs";
-import { mkdir } from "node:fs/promises";
+import { mkdir, writeFile } from "node:fs/promises";
 import { dirname } from "node:path";
 import { Readable } from "node:stream";
 import { pipeline } from "node:stream/promises";
@@ -11,6 +11,8 @@ const repository = "google/dawn";
 const platform = "ubuntu-latest-Release";
 const envFile = ".env.local";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 process.loadEnvFile(envFile);
 
 const githubToken = process.env.GITHUB_TOKEN;
@@ -18,7 +20,7 @@ if (!githubToken) {
   throw new Error("GITHUB_TOKEN is not set in the environment variables.");
 }
 
-function fetchDawnGitHub(relativeUrl: string) {
+function fetchFromDawnRepo(relativeUrl: string) {
   return fetch(`https://api.github.com/repos/${repository}/${relativeUrl}`, {
     headers: { Authorization: `Bearer ${githubToken}` },
   });
@@ -26,7 +28,7 @@ function fetchDawnGitHub(relativeUrl: string) {
 
 async function fetchTags() {
   console.log("Fetching Dawn release tags...");
-  const response = await fetchDawnGitHub("tags");
+  const response = await fetchFromDawnRepo("tags");
   const tags = await response.json();
   console.log(`Found ${tags.length} tags, checking for artifacts...`);
   return tags;
@@ -35,7 +37,7 @@ async function fetchTags() {
 async function findArtifactForTag(tag: any) {
   process.stdout.write(`Checking ${tag.name}... `);
 
-  const response = await fetchDawnGitHub(
+  const response = await fetchFromDawnRepo(
     `actions/artifacts?name=Dawn-${tag.commit.sha}-${platform}`
   );
   const artifacts = await response.json();
@@ -52,8 +54,7 @@ async function findArtifactForTag(tag: any) {
 }
 
 async function downloadArtifact(artifact: any) {
-  console.log("Downloading...");
-  const response = await fetchDawnGitHub(
+  const response = await fetchFromDawnRepo(
     `actions/artifacts/${artifact.id}/zip`
   );
 
@@ -61,8 +62,6 @@ async function downloadArtifact(artifact: any) {
     throw new Error(`Failed to download artifact: ${response.statusText}`);
   }
 
-  const __dirname = dirname(fileURLToPath(import.meta.url));
-  await mkdir(`${__dirname}/${downloadDir}`, { recursive: true });
   await pipeline(
     Readable.fromWeb(response.body as ReadableStream),
     createWriteStream(`${__dirname}/${downloadDir}/${artifact.name}.zip`)
@@ -71,16 +70,28 @@ async function downloadArtifact(artifact: any) {
   console.log(`Downloaded: ${artifact.name}.zip`);
 }
 
-async function downloadLatestDawnArtifact() {
+async function downloadDawnJson(tag: any) {
+  const response = await fetchFromDawnRepo(
+    `contents/src/dawn/dawn.json?ref=${tag.commit.sha}`
+  );
+  const dawnJson = await response.json();
+  const content = Buffer.from(dawnJson.content, "base64");
+  await writeFile(`${__dirname}/${downloadDir}/dawn.json`, content);
+  console.log("Downloaded: dawn.json");
+}
+
+async function downloadLatestDawn() {
   const tags = await fetchTags();
 
   for (const tag of tags) {
     const artifact = await findArtifactForTag(tag);
     if (artifact) {
-      await downloadArtifact(artifact);
+      console.log("Downloading...");
+      await mkdir(`${__dirname}/${downloadDir}`, { recursive: true });
+      await Promise.all([downloadArtifact(artifact), downloadDawnJson(tag)]);
       break;
     }
   }
 }
 
-downloadLatestDawnArtifact();
+downloadLatestDawn();
